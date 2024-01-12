fbecd17730c672e58b96c00f30cf3e993a6f3120
Omnia711 <152023450+Omnia711@users.noreply.github.com>
2024-01-12 23:12:29 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -69,12 +69,5 @@ function login() {
     }
 }
 
-// welcome page (home.html)
-var sessionUsername = localStorage.getItem('sessionUsername');
 
-if (sessionUsername) {
-    var welcomeUsername = document.getElementById('username');
-    welcomeUsername.textContent = 'Welcome ' + sessionUsername;
-} else {
-    location.replace('index.html');}
 
